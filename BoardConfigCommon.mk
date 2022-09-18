e9b8d1eeb2a0380c1ef09de06e9586ed57e99e16
jmpfbmx <jmpf.bmx@gmail.com>
2022-09-18 11:38:43 +0200

    msm8953-common: BoardConfigCommon: Adjust Kernel Section for AOSPA [1/2]
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ia48450249a9e00d07aa34d8748e6f49b8d717230

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,14 +133,11 @@ TARGET_PROVIDES_KEYMASTER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM,115200,n8 androidboot.console=ttyMSM androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/bq/msm8953
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_VERSION := 4.9
+KERNEL_CUSTOM_LLVM := true
+KERNEL_SD_LLVM_SUPPORT := false
 
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
