8ddacde1808195dd8a78114f743921126a15e52c
Jake Weinstein <jake@aospa.co>
2023-03-16 13:21:00 +0100

    msm8953-common: move TARGET_BOARD_PLATFORM to device.mk
    This lets us check for SoC in components.
    
    Change-Id: Ic50c75ad759e82bef5f067389ac15f8ae0f2cd5f

--- msm8953.mk
+++ msm8953.mk
@@ -371,6 +371,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
+# Platform
+TARGET_BOARD_PLATFORM := msm8953
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
